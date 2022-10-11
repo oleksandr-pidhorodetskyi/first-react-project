4bfde7529305562d4cbb607e369e494f37b20362
Oleksandr Pidhorodetskyi <s.pidgorodetsky@gmail.com>
2022-10-11 11:41:38 +0300

    Not finshed navbar

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <div className="container pt-4">
+        <Routes>
+          <Route path={'/'} element={<Home/>} />
+          <Route path={'/about'} element={<About/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
